aeedb8168bcda4adb0c73e077048cf3812445dd6
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-01-25 08:25:29 -0800

    Agregando Enums

---
+++ BeerStore/Enum/PresentationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum PresentationType
+{
+        Late,
+        Six_pack,
+        Bottle,
+        Barriel
+  }
+
